759d36f340f9929c55e35a1fd69d84a3ee76283a
Daniel Waters <danwater1@gmail.com>
2019-03-25 17:57:54 -0700

    Tests for DB connections

--- test/main.test.js
+++ test/main.test.js
@@ -1,14 +1,8 @@
 require("dotenv").config();
-const { pgp } = require("../database/db");
+const { db, pgp } = require("../database/db");
 const utils = require("../database/utils");
 
-QUnit.begin(() => {
-
-})
-
-
 QUnit.done(() => {
-  console.log(process.env);
   // Reset everything back and close database connection
   utils.rebuildData().then(() => pgp.end());
 });
