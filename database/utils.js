6d73e9040f06170283e63c525c9fa8659f96719f
Daniel Waters <danwater1@gmail.com>
2019-03-25 18:04:20 -0700

    Added tests for utils (build/delete) tables

--- database/utils.js
+++ database/utils.js
@@ -1,26 +1,30 @@
-const { db } = require('./db');
-const sql = require('./sql');
+const { db } = require("./db");
+const sql = require("./sql");
 
 const rebuildData = () => {
   return clearDatabase()
     .then(() => createTables())
-    .catch(err => console.log('Error creating DB with err ' + err))
-}
+    .catch(err => console.log("Error creating DB with err " + err));
+};
 
 const clearDatabase = () => {
-  return db.none(sql.stats.dropTable)
-  .then(() => db.none(sql.videos.dropTable))
-  .then(() => db.none(sql.workouts.dropTable))
-  .then(() => db.none(sql.users.dropTable))
-}
+  return db
+    .none(sql.stats.dropTable)
+    .then(() => db.none(sql.videos.dropTable))
+    .then(() => db.none(sql.workouts.dropTable))
+    .then(() => db.none(sql.users.dropTable));
+};
 
 const createTables = () => {
-  return db.none(sql.users.createTable)
-  .then(() => db.none(sql.workouts.createTable))
-  .then(() => db.none(sql.videos.createTable))
-  .then(() => db.none(sql.stats.createTable))
-}
+  return db
+    .none(sql.users.createTable)
+    .then(() => db.none(sql.workouts.createTable))
+    .then(() => db.none(sql.videos.createTable))
+    .then(() => db.none(sql.stats.createTable));
+};
 
 module.exports = {
-  rebuildData
-}
+  rebuildData,
+  clearDatabase,
+  createTables
+};
--- test/database/utils.test.js
+++ test/database/utils.test.js
@@ -29,6 +29,61 @@ QUnit.module("Database Util Testing", {
 });
 
 QUnit.test("Checks that Database Utils Exists", assert => {
-
   assert.ok(utils !== null);
+});
+
+QUnit.test("Checks that clears tables", assert => {
+  const checkTableAmount = assert.async();
+  const checkClearedAllTables = assert.async();
+  db.any(
+    "SELECT COUNT(table_name) FROM information_schema.tables " +
+      "WHERE table_schema='public'"
+  )
+    .then(result => {
+      assert.equal(result[0].count, 4, `Current number of tables is 4`);
+      checkTableAmount();
+      return utils.clearDatabase();
+    })
+    .then(result => {
+      return db.any(
+        "SELECT COUNT(table_name) FROM " +
+          "information_schema.tables WHERE table_schema='public'"
+      );
+    })
+    .then(result => {
+      checkClearedAllTables();
+      assert.equal(result[0].count, "0", "Tables is 0");
+    })
+    .catch(error => {
+      checkTableAmount();
+      checkClearedAllTables();
+      console.log(error);
+      assert.ok(false, "Unable to clear tables");
+    });
+
+  assert.ok(1 == 1);
+});
+
+QUnit.test("Checks that tables are created", assert => {
+  const asyncCreateTables = assert.async();
+  utils
+    .clearDatabase()
+    .then(() => {
+      return utils.createTables();
+    })
+    .then(() => {
+      return db.any(
+        "SELECT COUNT(table_name) " +
+          "FROM information_schema.tables " +
+          "WHERE table_schema='public'"
+      );
+    })
+    .then(result => {
+      asyncCreateTables();
+      assert.equal(result[0].count, 4, "Created tables");
+    })
+    .catch(err => {
+      asyncCreateTables();
+      assert.ok(false, "Unable to create tables");
+    });
 });
