c9fab4cd5060040c33013d78bdd1965a971ae97c
Daniel Waters <danwater1@gmail.com>
2019-04-05 10:39:24 -0700

    Changes recording_date to timestamp so can see the time when workout created

--- database/sql/workouts/createTable.sql
+++ database/sql/workouts/createTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE workouts (
   work_id SERIAL PRIMARY KEY,
   email VARCHAR (50) REFERENCES users(email),
-  recording_date DATE NOT NULL,
+  recording_date TIMESTAMP NOT NULL,
   workout_length INTEGER NOT NULL,
   num_of_intervals INTEGER NOT NULL,
   interval_length INTEGER NOT NULL
