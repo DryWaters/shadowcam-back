394ea734efdba2dcda304393e6e4b843916ef50e
danrachou <dan.rachou@gmail.com>
2019-04-08 09:18:36 -0700

    get single workout by work_id

--- routes/workout.js
+++ routes/workout.js
@@ -78,3 +78,40 @@ router.get(
       });
   }
 );
+
+// Get workouts by workout id
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+
+    work_id = { work_id: req.params.id }
+    const p1 = db.any(sql.workouts.getWorkoutByWorkID, work_id)
+    const p2 = db.any(sql.videos.getVideosByWorkID, work_id)
+    const p3 = db.any(sql.stats.getStatsByWorkID, work_id)
+
+    Promise.all([p1, p2, p3])
+    .then(result => {
+      if(result.length > 0){
+        res.json({
+          status: "ok",
+          message: {
+            workout: result[0],
+            videos: result[1],
+            stats: result[2]
+          }
+        })
+      } else {
+        res.json({
+          status: "ok",
+          message: "No results on workout id"
+        })
+      }
+    })
+    .catch(err => {
+      res.json({
+        status: "error",
+        message: `Select failed with error ${err}`
+      })
+    })
+  })
