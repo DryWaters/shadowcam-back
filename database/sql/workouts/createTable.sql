ef9d554c73c2368305a2b39ac3c80fdf128370a5
Daniel Waters <danwater1@gmail.com>
2019-04-27 13:19:42 -0700

    Adds time zone information for workouts

--- database/sql/workouts/createTable.sql
+++ database/sql/workouts/createTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE workouts (
   work_id SERIAL PRIMARY KEY,
   email VARCHAR (50) REFERENCES users(email),
-  recording_date TIMESTAMP NOT NULL,
+  recording_date TIMESTAMPTZ NOT NULL,
   workout_length INTEGER NOT NULL,
   num_of_intervals INTEGER NOT NULL,
   interval_length INTEGER NOT NULL
